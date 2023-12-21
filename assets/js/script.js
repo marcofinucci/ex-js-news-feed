4e04578910cd9307e0e1523c6b8fb6c8b795d55a
marcofinucci <info@marcofinucci.com>
2023-12-21 11:57:03 +0100

    init commit

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+const news = [
+    {
+        id: 1,
+        title: 'Scoperta di una nuova specie di papera di gomma',
+        content: 'Scoperta di una nuova specie di papera di gomma',
+        tags: ['geo', 'tech'],
+        author: 'Diana Rossi',
+        published: '2023-02-11',
+    },
+    {
+        id: 1,
+        title: 'Esplorando le profondità marine: il mistero degli abissi',
+        content: 'Esplorando le profondità marine: il mistero degli abissi',
+        tags: ['geo', 'viaggi'],
+        author: 'Fabio Mari',
+        published: '2023-03-14',
+    },
+    {
+        id: 1,
+        title: 'Viaggio culinario: alla ricerca dei sapori perduti',
+        content: 'Esplorazione di tradizioni culinarie dimenticate e la ricerca di sapori autentici.',
+        tags: ['cucina'],
+        author: 'Marta Bianchi',
+        published: '2023-04-20',
+    },
+    {
+        id: 1,
+        title: 'Arte moderna: oltre i confini convenzionali',
+        content: "Un'analisi delle tendenze e delle sfide nell'arte contemporanea, con interviste a artisti emergenti.",
+        tags: ['arte', 'tech'],
+        author: 'Gabriele Neri',
+        published: '2023-05-29',
+    }
+]
